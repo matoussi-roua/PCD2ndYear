ededc61e986a9f70d6128ecc58f84593a0fe75e0
matoussi roua <85041175+matoussi-roua@users.noreply.github.com>
2024-04-18 22:40:39 +0100

    correction commit

--- src/main/java/PCD/BACKEND/RECRAFTMARKET/dto/file/FileDto.java
+++
@@ -1,9 +0,0 @@
-package PCD.BACKEND.RECRAFTMARKET.dto.file;
-
-
-
-public record FileDto (long id,
-                       String name,
-                       String type,
-                       String filePath){
-}
